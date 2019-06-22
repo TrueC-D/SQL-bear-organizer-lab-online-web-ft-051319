77d27b61688b19daeaad6f45f45b810476571d55
Alethea Cary <aletheacary@gmail.com>
2019-06-22 02:21:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,10 @@
 INSERT INTO cats (name, age, breed) VALUES ()
+
+Mr. Chocolate
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
+unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
