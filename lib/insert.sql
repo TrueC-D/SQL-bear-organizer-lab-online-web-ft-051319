7848663f73c46cf756a4972c9e243e878809a72d
Alethea Cary <aletheacary@gmail.com>
2019-06-22 02:30:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,21 +9,21 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (8,null, 20, "M", "black", "aggressive", 0);
 
 
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
+-- INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate")
 
--- CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INT, gender TEXT, color TEXT, temperament TEXT, alive NUMERIC);
+-- -- CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INT, gender TEXT, color TEXT, temperament TEXT, alive NUMERIC);
 
 
-Rowdy
-Tabitha
-Sergeant Brown
-Melissa
-Grinch
-Wendy
-unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
